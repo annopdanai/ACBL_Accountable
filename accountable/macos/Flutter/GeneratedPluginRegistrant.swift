57f6ed4ddadd8b5e96a59b352b83da3004fae960
Anucha Chewachanon <68524576+SpiralNuggets@users.noreply.github.com>
2025-03-20 22:17:54 +0700

    feat: added Firebase into the project

--- accountable/macos/Flutter/GeneratedPluginRegistrant.swift
+++ accountable/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,13 @@
 import FlutterMacOS
 import Foundation
 
+import cloud_firestore
 import file_picker
 import firebase_core
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
